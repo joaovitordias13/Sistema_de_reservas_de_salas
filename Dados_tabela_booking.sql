831c89395244f836cd0b2379ca7b80020c0e400d
Jackson Felipe do Nascimento Santos <jacksonfelipe.contact@gmail.com>
2026-02-17 18:08:08 -0300

    PLAT-1886 = Adiciona dados de reservas para as salas no banco de dados.

---
+++ Dados_tabela_booking.sql
@@ -0,0 +1,172 @@
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 1: Sala Falcão - 7º Andar, feita por Felipe Moreira id 1 (Produtos id 4) com 20 participantes
+VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 198),
+(1, 9),
+(1, 10),
+(1, 11),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 200),
+(1, 199),
+(1, 17),
+(1, 18),
+(1, 19),
+(1, 20);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 2: Sala águia - 4º Andar, feita por Zenilda Rocha id 2 (Recursos Humanos id 50) com 9 participantes
+VALUES
+(2, 50),
+(2, 21),
+(2, 22),
+(2, 23),
+(2, 24),
+(2, 25),
+(2, 26),
+(2, 27),
+(2, 28);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 3: Sala Quero-quero - 7º Andar, feita por Edmundo Lima id 55 com 11 participantes
+VALUES
+(3, 55),
+(3, 29),
+(3, 30),
+(3, 31),
+(3, 32),
+(3, 33),
+(3, 34),
+(3, 35),
+(3, 196),
+(3, 37),
+(3, 197);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 4: Sala Tucano - 7º Andar, feita por Pedro Araujo id 15 com 13 participantes
+VALUES
+(4, 15),
+(4, 38),
+(4, 39),
+(4, 40),
+(4, 41),
+(4, 42),
+(4, 43),
+(4, 44),
+(4, 195),
+(4, 46),
+(4, 47),
+(4, 48),
+(4, 49);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 5: Sala Pica-pau - 2º Andar, feita por Sofia Mendes id 16 com 12 participantes
+VALUES
+(5, 16),
+(5, 194),
+(5, 51),
+(5, 52),
+(5, 53),
+(5, 54),
+(5, 193),
+(5, 56),
+(5, 57),
+(5, 58),
+(5, 59),
+(5, 60);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 6: Sala Cacau - 2º Andar, feita por Xerxes Duarte id 100 com 16 participantes
+VALUES
+(6, 100),
+(6, 61),
+(6, 62),
+(6, 63),
+(6, 64),
+(6, 65),
+(6, 66),
+(6, 67),
+(6, 68),
+(6, 69),
+(6, 70),
+(6, 71),
+(6, 72),
+(6, 73),
+(6, 74),
+(6, 75);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 7: Sala Piu-piu - 2º Andar, feita por Samara Oliveira id 121 com 18 participantes
+VALUES
+(7, 121),
+(7, 76),
+(7, 77),
+(7, 78),
+(7, 79),
+(7, 80),
+(7, 81),
+(7, 82),
+(7, 83),
+(7, 84),
+(7, 85),
+(7, 86),
+(7, 87),
+(7, 88),
+(7, 89),
+(7, 90),
+(7, 91),
+(7, 92);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 8: Sala Bem-te-vi - 7º Andar, feita por Helena Almeida id 8 com 10 participantes
+VALUES
+(8, 8),
+(8, 93),
+(8, 94),
+(8, 95),
+(8, 96),
+(8, 97),
+(8, 98),
+(8, 99),
+(8, 101),
+(8, 102);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 9: Sala Tico-Tico - 4º Andar, feita por Ulisses Nunes id 45 com 9 participantes
+VALUES
+(9, 45),
+(9, 103),
+(9, 104),
+(9, 105),
+(9, 106),
+(9, 107),
+(9, 108),
+(9, 109),
+(9, 110);
+
+INSERT INTO booking (reserva_id, usuario_id)
+-- Reserva 10: Sala Arara - 7º Andar, feita por Leonardo Costa id 36 com 15 participantes
+VALUES
+(10, 36),
+(10, 111),
+(10, 112),
+(10, 113),
+(10, 114),
+(10, 115),
+(10, 116),
+(10, 117),
+(10, 118),
+(10, 119),
+(10, 120),
+(10, 122),
+(10, 123),
+(10, 124),
+(10, 125);
